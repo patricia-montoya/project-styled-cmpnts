361cae401abd34ad2fb2fd6224c7d3a4b0e25752
Patricia Montoya <patmonhin@gmail.com>
2019-07-09 14:54:37 -0500

    Adding buttons and using as prop

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Hero from '../globals/Hero'
 import Banner from '../globals/Banner'
+import { PrimaryBtn } from '../globals/Buttons'
 import homeImg from '../../images/homeBcg.jpeg'
+
 
 
 const Header = () => {
@@ -11,8 +13,11 @@ const Header = () => {
                 greeting="Welcome to" 
                 title="BeachWalk Resort"
                 text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                Ab rem tempora modi repudiandae, quos molestiae."/>
-                
+                Ab rem tempora modi repudiandae, quos molestiae.">
+
+                <PrimaryBtn t="1rem">View Details</PrimaryBtn>
+                {/* <PrimaryBtn as="a" href="http://www.google.com">View Details</PrimaryBtn> */}
+            </Banner>
         </Hero>
     )
 }
