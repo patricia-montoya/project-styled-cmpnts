3828fef72a69913236298ec1d175cf14040e6a59
Patricia Montoya <patmonhin@gmail.com>
2019-07-09 14:23:18 -0500

    Adding the banner and css helper functions

--- src/components/globals/GlobalStyles.js
+++ src/components/globals/GlobalStyles.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
+import { setColor, setFont } from '../../styles'
 
 const Globals = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css?family=Courgette|Lato:400,700&display=swap');
@@ -11,10 +12,9 @@ const Globals = createGlobalStyle`
 
     body {
         font-size: 100%;
-        color: #222;
-        background: #fff;
-        font-family: 'Lato', sans-serif;
-        font-family: 'Courgette', cursive;
+        color: ${setColor.mainBlack};
+        background: ${setColor.mainWhite};
+        ${setFont.main}
     }
 
     h1 {
--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import Hero from '../globals/Hero'
+import Banner from '../globals/Banner'
+import homeImg from '../../images/homeBcg.jpeg'
+
 
 const Header = () => {
     return (
-        <div>
-            Hello from Header!
-        </div>
+        <Hero img={homeImg}>
+            <Banner 
+                greeting="Welcome to" 
+                title="BeachWalk Resort"
+                text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Ab rem tempora modi repudiandae, quos molestiae."/>
+                
+        </Hero>
     )
 }
 
